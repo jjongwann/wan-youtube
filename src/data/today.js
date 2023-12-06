17de6c5201c5ca162f9aea773c7d337306e48531
Jongs <143797909+jjongwann@users.noreply.github.com>
2023-12-06 18:22:51 +0900

    추천 개발자 작업

---
+++ src/data/today.js
@@ -0,0 +1,76 @@
+import today01 from "../assets/img/today/today01.png";
+import today02 from "../assets/img/today/today02.png";
+import today03 from "../assets/img/today/today03.png";
+import today04 from "../assets/img/today/today04.png";
+import today05 from "../assets/img/today/today05.png";
+import today06 from "../assets/img/today/today06.png";
+import today07 from "../assets/img/today/today07.png";
+import today08 from "../assets/img/today/today08.png";
+
+export const todayText = [
+    {
+        img: today01,
+        title: "[𝐏𝐋𝐀𝐘𝐋𝐈𝐒𝐓] 지나가던 산타도 듣고 간다는 크리스마스 캐롤 모음 🎄 크리스마스 캐롤",
+        desc : "🎄크리스마스 음악을 경험하는 즐거움을 공유하기 위해 이 채널을 찾아주셔서 감 크리스마스 음악은 다양한 감정과 이야기를 담은 멋진 곡들로 가득 차 있습니다.🎄",
+        author : "하얀 눈",
+        channelId: "UCKHV4MGtdlMb3aCGotUjzsQ",
+        date: "2023.12.06",
+        page: "/carol"
+    },{
+        img: today02,
+        title: "첫 눈 , 겨울의 분위기를 듬뿍 담은 노래 모음",
+        desc : "독특한 겨울 분위기를 즐기는 동안, 가족과 친구와 함께 특별한 순 우리 채널은 여러분의 겨울을 더욱 따뜻하게 만들기 위해 여기 있습니다 . 함께 해서 겨울 음악의 감동으로 하루를 채워보세요.  메리 크리스마스! ❄️❄️",
+        author : "하얀 눈",
+        channelId: "UCKHV4MGtdlMb3aCGotUjzsQ",
+        date: "2023.12.06",
+        page: "/carol"
+    },{
+        img: today03,
+        title: "[𝐏𝐋𝐀𝐘𝐋𝐈𝐒𝐓] 크리스마스 기다리며 소원 적고가면 다 이루어진다🎄🎅🏻",
+        desc : "🎄❄️연말이 다가오면 우리는 세대를 초월한 축제의 멜로디와 따뜻한 가사를 담아낸 음악의 세계로 여러분을 초대합니다.이 노래들은 우리를 옛날로 데려가 기억에 잠긴 감정을 일깨워주며, 이 시즌의 진정한 정신을 떠올리게 합니다. 🎄🎅",
+        author : "하얀 눈",
+        channelId: "UCHFxbyVOxEPA2CSPBVKjUvg",
+        date: "2023.12.06",
+        page: "/carol"
+    },{
+        img: today04,
+        title: "[𝐏𝐋𝐀𝐘𝐋𝐈𝐒𝐓] 평생 듣고 싶은 곡으로만 엄선한 올드 감성 크리스마스 캐롤🎄🎅 퍼펙트 크리스마스 캐롤 플레이리스트 ",
+        desc : "🎁겨울 음악의 아름다운 세 추위와 함께 따스 한 감정을 느끼고, 황홀한 음악을 통해 겨울의 아름다움을 누려보세요. 🎶독특한 겨울 분위기를 즐기는 동안, 가족과 친구와 함께 특별한 순 우리 채널은 여러분의 겨울을 더욱 따뜻하게 만들기 위해 여기 있습니다 .",
+        author : "하얀 눈",
+        channelId: "UCHFxbyVOxEPA2CSPBVKjUvg",
+        date: "2023.12.06",
+        page: "/carol"
+    },{
+        img: today05,
+        title: "매일매일 열심히 공부하게 만드는 원동력 | 5 hour lofi hip hop / study / work / chill beats",
+        desc : "💕매일매일 열심히 공부하게 만드는 원동력 | 5 hour lofi hip hop / study / work / chill beats",
+        author : "Lofi코딩",
+        channelId: "UCZ1s3hc2U9AOcIsHwIa3DQw",
+        date: "2023.12.06",
+        page: "/lofi"
+    },{
+        img: today06,
+        title: "코딩할때 듣기 좋은 노래 • 3 hours playlist • lofi type beat • beats to relax • study music",
+        desc : "These songs to boost your mood • 3 hours lofi hip hop mix / beats to relax / study to",
+        author : "Lofi코딩",
+        channelId: "UC-9-kyTW8ZkZNDHQJ6FgpwQ",
+        date: "2023.12.06",
+        page: "/lofi"
+    },{
+        img: today07,
+        title: "공부하고 일할 때 꼭 필요한 음악 | 3 hour lofi hip hop mix / lofi study / work / chill beats",
+        desc : "Dreamscape & Lofi Chill - Calm & Chill Lofi Music for Study , Coding , Work",
+        author : "Lofi코딩",
+        channelId: "UCkwegMjATseS7obo5GW_YsQ",
+        date: "2023.12.06",
+        page: "/lofi"
+    },{
+        img: today08,
+        title: "[ 𝑷𝒍𝒂𝒚𝒍𝒊𝒔𝒕 ] 코딩할때 듣기 좋은 노래 | 3 hour playlist | Lofi hip hop mix ~ jazzhop ~ relax beats",
+        desc : "Good music to listen to while study and coding | 3 hour lofi hiphop mix / jazzhop",
+        author : "Lofi코딩",
+        channelId: "UCrOw0E3-URvN_y54hX9Jg6Q",
+        date: "2023.12.06",
+        page: "/lofi"
+    },
+]
